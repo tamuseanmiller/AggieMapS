7bf2ac010a79f75fe95a68c68571c5bb7afc2bb0
Sean Miller <sean@millerfamily.tech>
2021-10-10 12:56:19 -0500

    Fixed a Few Issues, Check Description
    * Fixed the issue with the searchview changing the status bar icon color
    * Added some more searchview settings
    * Fixed issue with menu items not being the right color

--- app/src/main/java/com/mrst/aggiemaps/MainActivity.java
+++ app/src/main/java/com/mrst/aggiemaps/MainActivity.java
@@ -5,6 +5,8 @@ import static android.content.ContentValues.TAG;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.content.res.Configuration;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
@@ -85,8 +87,8 @@ public class MainActivity extends AppCompatActivity implements GISSearchAdapter.
     }
 
     private void requestFocusOnSearch() {
-        materialSearchView.setVisibility(View.VISIBLE);
         materialSearchView.requestFocus();
+        materialSearchView.setVisibility(View.VISIBLE);
         materialSearchBar.setVisibility(View.GONE);
         hideSystemUI();
     }
@@ -205,6 +207,11 @@ public class MainActivity extends AppCompatActivity implements GISSearchAdapter.
         materialSearchView.setVisibility(View.GONE);
         materialSearchView.setHint("Try Building Numbers/Names");
         materialSearchView.setBackgroundColor(ContextCompat.getColor(this, R.color.background));
+        Drawable clearIcon = ContextCompat.getDrawable(this, R.drawable.close);
+        clearIcon.setTintList(ColorStateList.valueOf(getColor(R.color.foreground)));
+        materialSearchView.setClearIcon(clearIcon);
+        materialSearchView.setDividerColor(ContextCompat.getColor(this, android.R.color.transparent));
+        materialSearchView.setTextClearOnBackPressed(true);
 
         // Create a new token for the autocomplete session. Pass this to FindAutocompletePredictionsRequest,
         // and once again when the user makes a selection (for example when calling fetchPlace()).
@@ -266,11 +273,13 @@ public class MainActivity extends AppCompatActivity implements GISSearchAdapter.
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.search_menu, menu);
+        menu.getItem(0).setIconTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.foreground)));
+        menu.getItem(1).setIconTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.foreground)));
         return true;
     }
 
     /*
-    * Handle menu actions
+     * Handle menu actions
      */
     @SuppressLint("NonConstantResourceId")
     @Override
@@ -279,7 +288,7 @@ public class MainActivity extends AppCompatActivity implements GISSearchAdapter.
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        switch(id) {
+        switch (id) {
             case R.id.action_microphone:
                 displaySpeechRecognizer();
                 break;
@@ -437,9 +446,7 @@ public class MainActivity extends AppCompatActivity implements GISSearchAdapter.
      */
     @Override
     public void onBackPressed() {
-        if (materialSearchView.hasFocus()) {
-            clearFocusOnSearch();
-        }
+        clearFocusOnSearch();
     }
 
     /*
@@ -447,10 +454,24 @@ public class MainActivity extends AppCompatActivity implements GISSearchAdapter.
      */
     private void hideSystemUI() {
         View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        int currentNightMode = Configuration.UI_MODE_NIGHT_MASK & getResources().getConfiguration().uiMode;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                // Night mode is not active, we're using the light theme
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                // Night mode is active, we're using dark theme
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+                break;
+        }
     }
 
     /*
@@ -458,10 +479,25 @@ public class MainActivity extends AppCompatActivity implements GISSearchAdapter.
      */
     private void showSystemUI() {
         View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        int currentNightMode = Configuration.UI_MODE_NIGHT_MASK & getResources().getConfiguration().uiMode;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                // Night mode is not active, we're using the light theme
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                // Night mode is active, we're using dark theme
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                break;
+        }
     }
 
     /*
